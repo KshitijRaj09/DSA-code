4fee4133a36b3a0a38a6330eca18d2e31f59b91e
Kshitij Raj <59702912+KshitijRaj09@users.noreply.github.com>
2024-01-18 23:58:35 +0530

    Create remove_duplicates_sorted_array.js

---
+++ remove_duplicates_sorted_array.js
@@ -0,0 +1,17 @@
+//Ques: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+var removeDuplicates = function(nums) {
+    if(!nums.length){
+        return 0;
+    }
+    let count = 1;
+    for(let i=1; i<nums.length; i++){
+        if(nums[count-1]!== nums[i]){
+            nums[count] = nums[i];
+            count++
+        }
+    }
+    return count;
+};
+
+reference: https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-Arrays/video/MTQzMw%3D%3D
