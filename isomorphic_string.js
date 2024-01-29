e6d12c1cedd7ffe21850ca6a2a1bada0dfa09eb8
Kshitij Raj <59702912+KshitijRaj09@users.noreply.github.com>
2024-01-29 17:43:50 +0530

    Create isomorphic_string.js

---
+++ isomorphic_string.js
@@ -0,0 +1,20 @@
+// ques : 205. Isomorphic Strings
+// https://leetcode.com/problems/isomorphic-strings/description/?envType=study-plan-v2&envId=top-interview-150
+
+var isIsomorphic = function(s, t) {
+    if(s.length !== t.length){
+        return false
+    }
+    const hashTable1={};
+    const hashTable2={}
+    for(let i=0; i<s.length; i++){
+        if(hashTable1[s[i]] !== hashTable2[t[i]]){
+            return false;
+        }
+        hashTable1[s[i]] = i+1;
+        hashTable2[t[i]] = i+1;
+    }
+    return true;
+};
+
+// trick match the indexes using hashtable.
