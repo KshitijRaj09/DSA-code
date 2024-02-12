71d7397d447cb02b9f42e6d9474b77610de5b317
Deep Shikha <47516502+Deep1Shikha@users.noreply.github.com>
2024-02-13 00:39:26 +0530

    Create majority_element.js

---
+++ majority_element.js
@@ -0,0 +1,18 @@
+//Ques : https://leetcode.com/problems/majority-element/description/?envType=daily-question&envId=2024-02-12
+// 169. Majority Element
+
+var majorityElement = function(nums) {
+    let count=0;
+    let element=nums[0];
+    for(let i in nums)
+        {
+            if(count===0)
+                element=nums[i]
+            
+            if(element===nums[i])
+                count++;
+            else
+                count--;
+        }
+    return element;
+};
