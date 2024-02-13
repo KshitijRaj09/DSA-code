50f06ace4526b3d40b8218542078d6bf8d93d4e7
Kshitij Raj <59702912+KshitijRaj09@users.noreply.github.com>
2024-02-14 00:19:05 +0530

    Create move_zeroes.js

---
+++ move_zeroes.js
@@ -0,0 +1,14 @@
+// Ques: 283. Move Zeroes
+// https://leetcode.com/problems/move-zeroes/description/
+
+var moveZeroes = function(nums) {
+   let countZero=0;
+   for(let i=0; i<nums.length; i++){
+       if(nums[i]!==0){
+           let temp = nums[i];
+           nums[i] = nums[countZero];
+           nums[countZero] = temp;
+           countZero++;
+       }
+   }
+};
