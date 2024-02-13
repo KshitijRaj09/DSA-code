d007839a3287a5b41cd1a715d7740446fe1351eb
Kshitij Raj <59702912+KshitijRaj09@users.noreply.github.com>
2024-02-13 22:15:22 +0530

    Update move_non_zeroes_left.js

--- move_non_zeroes_left.js
+++ move_non_zeroes_left.js
@@ -1,15 +1,15 @@
+// ques: // 283. Move Zeroes
+// https://leetcode.com/problems/move-zeroes/description/
 var moveZeroes = function(nums) {
-    //j check for zeroes and swap with non zeroes
-    let count=0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]!==0){
-            let temp = nums[i];
-            nums[i] = nums[count];
-            nums[count]= temp;
-            count++;
-        }
-    }
+   let countZero=0;
+   for(let i=0; i<nums.length; i++){
+       if(nums[i]!==0){
+           let temp = nums[i];
+           nums[i] = nums[countZero];
+           nums[countZero] = temp;
+           countZero++;
+       }
+   }
 };
-
 // link : https://leetcode.com/problems/move-zeroes
 // Help : GFG DSA course
