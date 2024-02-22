cbafa4d357b6603a7379115f6f6779f76799e711
Kshitij Raj <59702912+KshitijRaj09@users.noreply.github.com>
2024-02-22 20:15:30 +0530

    Create bitwise_number_range.js

---
+++ bitwise_number_range.js
@@ -0,0 +1,20 @@
+// Ques: 201. Bitwise AND of Numbers Range
+https://leetcode.com/problems/bitwise-and-of-numbers-range/description/?envType=daily-question&envId=2024-02-21
+
+var rangeBitwiseAnd = function(left, right) {
+    while(right>left){
+        right &= (right-1);
+    }
+    return right;
+};
+
+/*
+2. Brian Kernighan’s Algorithm: 
+Subtracting 1 from a decimal number flips all the bits after the rightmost set bit(which is 1) including the rightmost set bit. 
+for example : 
+10 in binary is 00001010 
+9 in binary is 00001001 
+8 in binary is 00001000 
+7 in binary is 00000111 
+So if we subtract a number by 1 and do it bitwise & with itself (n & (n-1)), we unset the rightmost set bit. If we do n & (n-1) in a loop and count the number of times the loop executes, we get the set bit count. 
+The beauty of this solution is the number of times it loops is equal to the number of set bits in a given integer. */
